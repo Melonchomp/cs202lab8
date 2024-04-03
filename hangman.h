241ea96c39a8fbb1153250f57a9609bc44efc466
Christopher Santiago Ramos Medina <cramosme@hydra2.eecs.utk.edu>
2024-04-03 12:01:56 -0400

    Added a couple private variables and an idea for the game

--- hangman.h
+++ hangman.h
@@ -1,3 +1,10 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+#include <iostream>
+#include <string>
+
+
 //This will be the header file for our hangman game
 // yes
 
@@ -5,6 +12,18 @@ class Hangman{
     public:
 
       // I think we'll need a multi map
+      /* What if we had files for each category that contained a bunch of words. We could do something similar to
+         to what we did at Pelli where we read from a file, and wrote to that same file. So basically as long as they keep 
+         playing we update the files so that the same word isnt used twice and when they choose to end the game, we reset
+         the files. We can either go down the list or randomize it.
+      */
+
     private:
 
+      std::string word; // the word that is going to be guessed
+      std::string category; // the general category that the word belongs to
+      int attempts; // could possibly use this to keep track of how many times the user has guessed incorrectly
+
 };
+
+#endif
