1aeb7c81d43d8b6063e1c4b3d11804ba32df406d
Christopher Santiago Ramos Medina <cramosme@hydra2.eecs.utk.edu>
2024-04-04 19:56:34 -0400

    Finished report and .h

--- hangman.h
+++ hangman.h
@@ -1,29 +1,50 @@
+/*
+ Ahmed Ghazi, Chris Ramos, Kevin Canas
+ Lab 8
+
+*/
+
+
+
 #ifndef HANGMAN_H
 #define HANGMAN_H
 
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
-
-//This will be the header file for our hangman game
-// yes
+using namespace std;
 
 class Hangman{
-    public:
+   public:
+      Hangman(); // defualt constructor
+      void displayMan(); // prints out the man dying
+      void startGame(); // prints out user interface
+      void Menu(); // displays main menu and game options, restart, end, difficulty level
+      void hints(); // will look through vector of characters available and the first one that it sees that is in the word will input into answer
+      string chooseWord( char ); // th
 
-      // I think we'll need a multi map
       /* What if we had files for each category that contained a bunch of words. We could do something similar to
          to what we did at Pelli where we read from a file, and wrote to that same file. So basically as long as they keep 
          playing we update the files so that the same word isnt used twice and when they choose to end the game, we reset
          the files. We can either go down the list or randomize it.
       */
 
-    private:
+      
 
-      std::string word; // the word that is going to be guessed
-      std::string category; // the general category that the word belongs to
+   private:
+
+      unordered_map < string, vector< string > > wordBank; //key is category, value is vector of words in that category
+      vector< char > charAvailable; // holds letters that havent been used
+      string word; // the word that is going to be guessed
+      string category; // the general category that the word belongs to
       int attempts; // could possibly use this to keep track of how many times the user has guessed incorrectly
-
+      int gamesWon; 
+      int gamesLost;
+      int points; // depends on how many attempts they had left
+      int hintsUsed;
+      int perfectGames; // games won without getting any wrong
 };
 
 #endif
