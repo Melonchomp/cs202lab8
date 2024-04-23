bd1d61d8b22cfda5feaccd4e35900e9a6afbb0f3
Christopher Santiago Ramos Medina <cramosme@hydra2.eecs.utk.edu>
2024-04-23 12:34:03 -0400

    working on matching the guess

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main(){
    Hangman t;
    int currAttempt = 0;
    string userDifficulty, guess, cat;
+   char letter;
+   bool wordGuessed = false;
 
    while( userDifficulty != "E" && userDifficulty != "M" && userDifficulty != "H" ){
       cout << "Choose a difficulty: ";
@@ -20,16 +22,36 @@ int main(){
    t.setDifficulty( userDifficulty );
    cat = t.chooseCat();
    t.setWord( t.chooseWord( cat ) );
+   guess.resize( t.getWord().size(), '_' );
 
-   cout << cat << endl;
-   cout << t.getWord() << endl;
+   cout << cat << ": " << t.getWord() << endl;
 
-   // while( currAttempt < t.getAttempts() ){
-      
-   //    t.displayMan( currAttempt );
-   //    currAttempt++;
+   t.displayMan( currAttempt );
 
-   // }
+   while( currAttempt < t.getAttempts() ){
+
+         cout << "Guess a letter: ";
+         cin >> letter;
+         letter = toupper( letter );
+         
+         size_t index = t.getWord().find( letter );
+
+         cout << "Letter found at index: " << index << endl;
+         if( index == string::npos ){
+            currAttempt++;
+            t.displayMan( currAttempt );
+         }
+         else{
+            guess[index] == letter;
+         }
+         cout << guess << endl;
+         if( guess == t.getWord() ){
+            cout << "Congrats you guessed the word" << endl;
+            wordGuessed = true;
+            break;
+         }
+         if( wordGuessed ) break;
+   }
    
    return 0;
 }
