6c1fcaeecf60b497561239a4ef4959d0a61ead4a
Christopher Santiago Ramos Medina <cramosme@hydra2.eecs.utk.edu>
2024-04-25 12:08:19 -0400

    added a few comments

--- hangman.h
+++ hangman.h
@@ -19,19 +19,18 @@ using namespace std;
 class Hangman{
    public:
       Hangman(); // defualt constructor
-      void displayMan( int n ); // prints out the man dying
+      void displayMan( int n ); // prints out the man dying - C
       void startGame(); // prints out user interface
       void menu(); // displays main menu and game options, restart, end, difficulty level
       void hints(); // will look through vector of characters available and the first one that it sees that is in the word will input into answer
-      void fillBank( const string, const string );
-      string chooseCat();
-      string chooseWord( string c ); // this will choose a word from the word bank based on the category
-      string getDifficulty() const { return difficulty; }
-      void setDifficulty( string d );
-      int getAttempts() const { return attempts; }
-      string getWord() const { return word; }
-      void setWord( string w ){ word = w; }
-
+      void fillBank( const string, const string ); // fills the map wordBank - C
+      string chooseCat(); // will choose one of the categories - C
+      string chooseWord( string c ); // this will choose a word from the word bank based on the category - C
+      string getDifficulty() const { return difficulty; } // - C
+      void setDifficulty( string d ); // Depending on diff entered will - C
+      int getAttempts() const { return attempts; } // - C
+      string getWord() const { return word; } // - C
+      void setWord( string w ){ word = w; } //- C
       /* What if we had files for each category that contained a bunch of words. We could do something similar to
          to what we did at Pelli where we read from a file, and wrote to that same file. So basically as long as they keep 
          playing we update the files so that the same word isnt used twice and when they choose to end the game, we reset
